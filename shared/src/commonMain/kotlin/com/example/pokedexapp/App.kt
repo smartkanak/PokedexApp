bc9a86d8c04c56b323ebca03ece8d8e936d2d46b
hoener <hamza.oener@adesso-mobile.de>
2023-09-10 16:55:59 +0200

    first api call results printed

--- shared/src/commonMain/kotlin/com/example/pokedexapp/App.kt
+++ shared/src/commonMain/kotlin/com/example/pokedexapp/App.kt
@@ -1,18 +1,41 @@
 package com.example.pokedexapp
 
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import com.example.pokedexapp.listview.data.model.PokemonList
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
 
 @Composable
 fun App() {
-    MaterialTheme {
-        PokedexApp()
-    }
+    PokedexApp()
 }
 
 @Composable
-fun PokedexApp() {
-    // center text hello world
-    Text("Hello World!")
+fun PokedexApp(
+    modifier: Modifier = Modifier
+) {
+    MaterialTheme {
+        LaunchedEffect(Unit) {
+            println(getImages())
+        }
+    }
+}
+
+val httpClient = HttpClient() {
+    install(ContentNegotiation) {
+        json()
+    }
+}
+
+suspend fun getImages(): PokemonList {
+    val pokemonList = httpClient
+        .get("https://pokeapi.co/api/v2/pokemon")
+        .body<PokemonList>()
+    return pokemonList
 }
---
+++ shared/src/commonMain/kotlin/com/example/pokedexapp/listview/data/model/PokemonList.kt
@@ -0,0 +1,25 @@
+package com.example.pokedexapp.listview.data.model
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PokemonList(
+    @SerialName("count")
+    val count: Int?,
+    @SerialName("next")
+    val next: String?,
+    @SerialName("previous")
+    val previous: String?,
+    @SerialName("results")
+    val pokemonPointers: List<PokemonPointer?>?
+) {
+    @Serializable
+    data class PokemonPointer(
+        @SerialName("name")
+        val name: String?,
+        @SerialName("url")
+        val url: String?
+    )
+}
